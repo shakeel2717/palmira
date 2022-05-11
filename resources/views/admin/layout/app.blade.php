a3e64fe6bafebf23dc45febc3b9fd2e368749688
Shakeel Ahmad <shakeel2717@gmail.com>
2022-05-11 06:19:37 -0700

    nav user profile update

--- resources/views/admin/layout/app.blade.php
+++ resources/views/admin/layout/app.blade.php
@@ -211,7 +211,7 @@
                                 data-bs-toggle="dropdown" aria-expanded="false" data-bs-auto-close="outside"
                                 data-bs-dropdown-animation>
                                 <div class="avatar avatar-sm avatar-circle">
-                                    <img class="avatar-img" src="/assets/img/160x160/img6.jpg"
+                                    <img class="avatar-img" src="/assets/img/160x160/user.jpg"
                                         alt="Image Description">
                                     <span class="avatar-status avatar-sm-status avatar-status-success"></span>
                                 </div>
--- resources/views/user/layout/app.blade.php
+++ resources/views/user/layout/app.blade.php
@@ -211,7 +211,7 @@
                                 data-bs-toggle="dropdown" aria-expanded="false" data-bs-auto-close="outside"
                                 data-bs-dropdown-animation>
                                 <div class="avatar avatar-sm avatar-circle">
-                                    <img class="avatar-img" src="/assets/img/160x160/img6.jpg"
+                                    <img class="avatar-img" src="/assets/img/160x160/user.jpg"
                                         alt="Image Description">
                                     <span class="avatar-status avatar-sm-status avatar-status-success"></span>
                                 </div>
@@ -222,7 +222,7 @@
                                 <div class="dropdown-item-text">
                                     <div class="d-flex align-items-center">
                                         <div class="avatar avatar-sm avatar-circle">
-                                            <img class="avatar-img" src="/assets/img/160x160/img6.jpg"
+                                            <img class="avatar-img" src="/assets/img/160x160/user.jpg"
                                                 alt="Image Description">
                                         </div>
                                         <div class="flex-grow-1 ms-3">
